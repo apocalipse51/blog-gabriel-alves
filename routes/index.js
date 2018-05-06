83fdbb951e2dbb18819b387112a98f332a11595c
apocalipse51 <gabrielantonio20111@hotmail.com>
2018-05-05 23:06:25 -0300

    Adição do primeiro artigo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  	res.render('index', { title: 'Express' });
+});
+
+router.get('/angular', (req, res, next) => {
+	res.render('angular');
 });
 
 module.exports = router;
