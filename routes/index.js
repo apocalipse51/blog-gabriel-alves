c8ad3140c578c23f8df39e6881eb89dbc1155825
apocalipse51 <gabrielantonio20111@hotmail.com>
2018-05-05 23:08:26 -0300

    Correção do script de arranque

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   	res.render('index', { title: 'Express' });
 });
 
-router.get('/angular', (req, res, next) => {
-	res.render('angular');
-});
-
 module.exports = router;
